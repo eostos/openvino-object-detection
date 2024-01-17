377a660faa3fcbd6911459dbf630eb3d099dbeef
jcaltamare <jcaltamare>
2024-01-17 11:45:11 -0500

    Changes to be committed: 	modified:   cuda_object_detection.py

--- cuda_object_detection.py
+++ cuda_object_detection.py
@@ -10,6 +10,22 @@ import time
 import argparse
 
 import cv2
+
+import sort
+import util
+import time
+import numpy as np
+import json
+import redis
+from cls.clsDevice import Device
+import traceback
+import os
+import threading
+from httpOCRpy.server import OCR 
+import image_service_pb2
+import image_service_pb2_grpc
+import grpc
+
 import pycuda.autoinit  # This is needed for initializing CUDA driver
 
 from tensorrt_demos.utils.yolo_classes import get_cls_dict
@@ -18,8 +34,15 @@ from tensorrt_demos.utils.display import open_window, set_display, show_fps
 from tensorrt_demos.utils.visualization import BBoxVisualization
 from tensorrt_demos.utils.yolo_with_plugins import TrtYOLO
 
+tracker = sort.SORT(max_age=3, min_hits=3, iou_threshold=0.1)
 
 WINDOW_NAME = 'TrtYOLODemo'
+
+def is_running_in_docker():
+    # Docker crea un archivo .dockerenv en la raíz del sistema de archivos del contenedor.
+    # La presencia de este archivo puede ser un buen indicador.
+    path = "/.dockerenv"
+    return os.path.exists(path)
 
 
 def parse_args():
@@ -36,7 +59,7 @@ def parse_args():
         '-t', '--conf_thresh', type=float, default=0.3,
         help='set the detection confidence threshold')
     parser.add_argument(
-        '-m', '--model', type=str, required=True,
+        '-m', '--model', type=str, default="yolov4-tiny-288",
         help=('[yolov3-tiny|yolov3|yolov3-spp|yolov4-tiny|yolov4|'
               'yolov4-csp|yolov4x-mish|yolov4-p5]-[{dimension}], where '
               '{dimension} could be either a single number (e.g. '
@@ -48,7 +71,7 @@ def parse_args():
     return args
 
 
-def loop_and_detect(cam, trt_yolo, conf_th, vis):
+def loop_and_detect(cam, trt_yolo, conf_th, vis, config,connect_redis,device):
     """Continuously capture images from camera and do object detection.
 pip3 install
     # Arguments
@@ -60,8 +83,8 @@ pip3 install
     full_scrn = False
     fps = 0.0
     tic = time.time()
-    url="rtsp://Admin:1234@172.172.3.9/stream1"
-    cap = cv2.VideoCapture(url)
+    
+    cap = cv2.VideoCapture(config["vid_path"])
     if not cap.isOpened():
         print("Error: Could not open video.")
         exit()
@@ -76,12 +99,15 @@ pip3 install
         #    break
         boxes, confs, clss = trt_yolo.detect(img, conf_th)
         print("boxes ",boxes)
+        
+        
         img = vis.draw_bboxes(img, boxes, confs, clss)
         img = show_fps(img, fps)
+        util.send_video(img,connect_redis,config["device_id"])
         print(len(boxes))
-        if len(boxes) >= 1 :
-           cv2.imwrite('output_image_'+str(cont)+".jpg", img)
-           print('Image saved successfully.')   
+        #if len(boxes) >= 1 :
+        #   cv2.imwrite('output_image_'+str(cont)+".jpg", img)
+        #   print('Image saved successfully.')   
         #cv2.imshow(WINDOW_NAME, img)
         toc = time.time()
         curr_fps = 1.0 / (toc - tic)
@@ -98,14 +124,49 @@ pip3 install
 
 def main():
     args = parse_args()
+    stub=None
+    channel=None
+    
+    if is_running_in_docker():
+        ConfParams = util.getConfigs('/opt/alice-lpr-cpu/config.json',True)
+    else:
+        ConfParams = util.getConfigs('./config.json')
+    if ConfParams:
+        print(ConfParams)
+        # Parse the JSON string into a dictionary
+    try:
+        conf_dict = json.loads(ConfParams)
+        device = Device(conf_dict,util.send_video)
+        vid_path = conf_dict['vid_path']
+        debug = conf_dict['debug']
+        ip_redis = conf_dict['ip_redis']
+        port_redis = conf_dict['port_redis']
+        device_id = conf_dict['device_id']
+        country = conf_dict['country']
+        devicearg  = conf_dict['device']
+        ocr_grcp_ip = conf_dict['ocr_grcp_ip']
+        ocr_grcp_port = conf_dict['ocr_grcp_port']
+        ocr_grcp        = conf_dict['ocr_grcp']
+    except json.JSONDecodeError:
+        print("Error: Failed to parse the configuration parameters.")
+    except KeyError:
+        print("Error: 'vid_path' not found in the configuration parameters.")
+    connect_redis= redis.Redis(host=ip_redis, port=port_redis)
+    ocr = OCR(country)
+    prediction=ocr.prediction
+    if ocr_grcp:
+        print('{}:{}'.format(ocr_grcp_ip,ocr_grcp_port))
+        channel = grpc.insecure_channel('{}:{}'.format(ocr_grcp_ip,ocr_grcp_port))
+        stub = image_service_pb2_grpc.ImageServiceStub(channel)
+        
     if args.category_num <= 0:
         raise SystemExit('ERROR: bad category_num (%d)!' % args.category_num)
     if not os.path.isfile('./models/%s.trt' % args.model):
         raise SystemExit('ERROR: file (models/%s.trt) not found!' % args.model)
 
-    cam = Camera(args)
-    if not cam.isOpened():
-        raise SystemExit('ERROR: failed to open camera!')
+    #cam = Camera(args)
+    #if not cam.isOpened():
+    #    raise SystemExit('ERROR: failed to open camera!')
 
     cls_dict = get_cls_dict(args.category_num)
     vis = BBoxVisualization(cls_dict)
@@ -114,9 +175,9 @@ def main():
    # open_window(
    #     WINDOW_NAME, 'Camera TensorRT YOLO Demo',
    #     cam.img_width, cam.img_height)
-    loop_and_detect(cam, trt_yolo, args.conf_thresh, vis=vis)
+    loop_and_detect(None, trt_yolo, args.conf_thresh, vis=vis,url=vid_path,connect_redis=connect_redis,device=device)
 
-    cam.release()
+    
     cv2.destroyAllWindows()
 
 
